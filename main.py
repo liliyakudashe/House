1e3b555f2d186b44646a4411cbc58c1fa10816be
Acer <oduvan4ik_888@mail.ru>
2023-12-16 22:31:58 +0300

    Перегрузка операторов

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-from modul import House
+from modul import House, Building
 
 
 house1 = House()
+house2 = House()
+print(house1 == house2)
 House.setNewNumberOfFloors(house1, 12)
+
+
+building1 = Building(4, "офис")
+building2 = Building(4, "офис")
+building3 = Building(8, "квартира")
+building4 = building1
+
+print(building1)
+
+bhg = building1 == building2
+print(building2 == building3)
+print(building1 == building4)
+print(bhg)
+
--- modul.py
+++ modul.py
@@ -1,5 +1,7 @@
 class House:
 
+    numberOfFloors = 10
+
     def __init__(self):
         self.numberOfFloors = 10
 
@@ -7,3 +9,22 @@ class House:
         self.numberOfFloors = floors
         print("Текущий этаж равен", self.numberOfFloors)
 
+    def printFloors(self):
+        for floor in range(1, self.numberOfFloors + 1):
+            print("Текущий этаж равен", floor)
+
+
+class Building:
+
+    def __init__(self, floors: int, building_type: str):
+        self.numberOfFloors = floors
+        self.buildingType = building_type
+
+    def __eq__(self, other):
+        if isinstance(other, Building):
+            return (self.numberOfFloors == other.numberOfFloors
+                    and self.buildingType == other.buildingType)
+        return False
+
+    def __str__(self):
+        return f'{self.buildingType}, {self.numberOfFloors}'
